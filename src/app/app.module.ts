4367205411ba139bc9f47fbd58ee8d69349b9fbf
mathzds <pedrofilhodev@gmail.com>
2024-10-07 11:02:39 -0300

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { UserModule } from 'src/core/user/user.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from 'src/core/auth/auth.module';
+import { EmailModule } from 'src/core/email/email.module';
 
 @Module({
-  imports: [UserModule, AuthModule,
+  imports: [UserModule, EmailModule, AuthModule,
     ThrottlerModule.forRoot([{
       ttl: 200,
       limit: 1,
